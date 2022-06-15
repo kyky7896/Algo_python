84a4865245e93161177cda0ba60d5c7ad34b4084
kyky7896 <77670592+kyky7896@users.noreply.github.com>
2022-06-16 00:41:48 +0900

    220616-algo

---
+++ 백준_일곱난쟁이.py
@@ -0,0 +1,9 @@
+from itertools import combinations
+import sys
+
+input=sys.stdin.readline
+height=[ int(input()) for i in range(9)]
+for h in combinations(height,7):
+    if sum(h)<100:
+        for i in sorted(h):
+            print(i)
