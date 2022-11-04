db3771a2c19dfffeab89e174b6e17ea77af62ee7
kyky7896 <kyky7896@gmail.com>
2022-11-04 17:08:33 +0900

    221104

---
+++ P_없는숫자더하기.py
@@ -0,0 +1,14 @@
+def solution(numbers):
+    numbers.sort() ## numbers를 sort해라
+    list_num=[i for i in range(0,10)] # 0부터 9까지를 list_num에 담아라 
+
+    while len(numbers)>0:
+      a=numbers.pop() ## 주어진 numbers를 pop한걸 a에 담고
+      print("pop한수",a)
+      if a in list_num: ##list_num에 있는가?
+        list_num.pop(a)
+    print(sum(list_num))
+    return sum(list_num)
+
+numbers=[1,2,3,4,6,7,8,0]
+solution(numbers)
